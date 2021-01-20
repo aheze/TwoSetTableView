9139e58e535a3e6cc4e2593a4a1c1c2e4e7db60e
Zheng <ahzzheng@gmail.com>
2021-01-20 13:41:46 -0800

    Initial Commit

---
+++ TwoSetTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoSetTableView
+//
+//  Created by Zheng on 1/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
